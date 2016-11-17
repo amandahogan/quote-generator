6bcf1369872fc57d27cb19e5e69aeb64bb687287
amandahogan <hogan.ans@gmail.com>
2016-11-17 13:11:25 +1300

    add css and html

---
+++ quote.js
@@ -0,0 +1,7 @@
+curl -X POST --include 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies' \
+  -H 'X-Mashape-Key: AKIVvpSNx1mshOsQ6B7dLHoJgQJ3p1MuAo4jsn2dSCDlm6Mjv4' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Accept: application/json'
+
+  document.addEventListener('click',)
+  
